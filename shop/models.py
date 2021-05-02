a8ffe0e3dc4f51369e8ee6a71f577aa6d93d726b
Blake Beran <bberan@unomaha.edu>
2021-05-02 01:24:57 -0500

    adjusted format
    adjusted code format in models.py

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Category(models.Model):
     name = models.CharField(max_length=200,
                             db_index=True)
@@ -16,8 +17,7 @@ class Category(models.Model):
         return self.name
 
     def get_absolute_url(self):
-            return reverse('shop:product_list_by_category',
-                           args=[self.slug])
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -42,8 +42,7 @@ class Product(models.Model):
         return self.name
 
     def get_absolute_url(self):
-            return reverse('shop:product_detail',
-                           args=[self.id, self.slug])
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
     def get_created(self):
         return self.created
